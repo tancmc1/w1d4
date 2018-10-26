9897bdd1915bb9e74f048cbf0f6672b0921adafd
Martin Lorenzo Tanchanco <tanchanco@gmail.com>
2018-10-26 01:18:04 +0000

    added variables in parent function and accessed it from the inner function

---
+++ loadedDie.js
@@ -0,0 +1,16 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var i = 0 - 1;
+
+  return function() {
+    i += 1;
+    return list[i];
+  }
+  return i;
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
